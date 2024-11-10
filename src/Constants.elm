b6b7dcea39763b9fdd7acae6a0bc6f330b488d30
Michael Bryzek <mbryzek@alum.mit.edu>
2024-11-10 13:21:18 -0500

    wip

--- src/Constants.elm
+++ src/Constants.elm
@@ -1,12 +1,6 @@
-module Constants exposing (logoImg, logoSrc)
+module Constants exposing (logoSrc)
 
-import Html exposing (Html, img)
-import Html.Attributes exposing (src, height)
 
 logoSrc : String
 logoSrc =
-  "https://raw.githubusercontent.com/mbryzek/hackathon/refs/heads/main/assets/bt-cs-logo.png"
-
-logoImg : Html msg
-logoImg =
-  img [ src logoSrc, height 150 ] []
+    "https://raw.githubusercontent.com/mbryzek/hackathon/refs/heads/main/assets/bt-cs-logo.png"
