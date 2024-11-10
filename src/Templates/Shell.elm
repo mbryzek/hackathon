c3fdb543894611c812fd65cea126b6ad547858e3
Michael Bryzek <mbryzek@alum.mit.edu>
2024-11-10 13:15:00 -0500

    wip

--- src/Templates/Shell.elm
+++ src/Templates/Shell.elm
@@ -56,39 +56,40 @@ topNavSections =
 notificationsAndProfile : Html msg
 notificationsAndProfile =
     div
-        [ Attr.class "ml-4 flex items-center md:ml-6"
+        [ Attr.class "ml-4 flex items-center md:ml-6" ] [
+            notifications , profileDropdown ]
+
+notifications : Html msg
+notifications =
+    button
+        [ Attr.type_ "button"
+        , Attr.class "relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
         ]
-        [ button
-            [ Attr.type_ "button"
-            , Attr.class "relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
+        [ span
+            [ Attr.class "absolute -inset-1.5"
             ]
-            [ span
-                [ Attr.class "absolute -inset-1.5"
+            []
+        , span
+            [ Attr.class "sr-only"
+            ]
+            [ text "View notifications" ]
+        , svg
+            [ SvgAttr.class "h-6 w-6"
+            , SvgAttr.fill "none"
+            , SvgAttr.viewBox "0 0 24 24"
+            , SvgAttr.strokeWidth "1.5"
+            , SvgAttr.stroke "currentColor"
+            , Attr.attribute "aria-hidden" "true"
+            , Attr.attribute "data-slot" "icon"
+            ]
+            [ path
+                [ SvgAttr.strokeLinecap "round"
+                , SvgAttr.strokeLinejoin "round"
+                , SvgAttr.d "M14.857 17.082a23.848 23.848 0 0 0 5.454-1.31A8.967 8.967 0 0 1 18 9.75V9A6 6 0 0 0 6 9v.75a8.967 8.967 0 0 1-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 0 1-5.714 0m5.714 0a3 3 0 1 1-5.714 0"
                 ]
                 []
-            , span
-                [ Attr.class "sr-only"
-                ]
-                [ text "View notifications" ]
-            , svg
-                [ SvgAttr.class "h-6 w-6"
-                , SvgAttr.fill "none"
-                , SvgAttr.viewBox "0 0 24 24"
-                , SvgAttr.strokeWidth "1.5"
-                , SvgAttr.stroke "currentColor"
-                , Attr.attribute "aria-hidden" "true"
-                , Attr.attribute "data-slot" "icon"
-                ]
-                [ path
-                    [ SvgAttr.strokeLinecap "round"
-                    , SvgAttr.strokeLinejoin "round"
-                    , SvgAttr.d "M14.857 17.082a23.848 23.848 0 0 0 5.454-1.31A8.967 8.967 0 0 1 18 9.75V9A6 6 0 0 0 6 9v.75a8.967 8.967 0 0 1-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 0 1-5.714 0m5.714 0a3 3 0 1 1-5.714 0"
-                    ]
-                    []
-                ]
             ]
-        , profileDropdown
-    ]
+        ]
 
 profileDropdown : Html msg
 profileDropdown =
