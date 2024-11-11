4c6bb41c9cbd86734f24731d445a68625e658525
Michael Bryzek <mbryzek@alum.mit.edu>
2024-11-11 10:31:13 -0500

    wip

--- src/Templates/Shell.elm
+++ src/Templates/Shell.elm
@@ -298,84 +298,7 @@ renderShell props contents =
                     [ Attr.class "space-y-1 px-2 pb-3 pt-2 sm:px-3"
                     ]
                     [ topNavSections props.url navLink ]
-                , div
-                    [ Attr.class "border-t border-gray-700 pb-3 pt-4"
-                    ]
-                    [ div
-                        [ Attr.class "flex items-center px-5"
-                        ]
-                        [ div
-                            [ Attr.class "shrink-0"
-                            ]
-                            [ img
-                                [ Attr.class "h-10 w-10 rounded-full"
-                                , Attr.src "https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                                , Attr.alt ""
-                                ]
-                                []
-                            ]
-                        , div
-                            [ Attr.class "ml-3"
-                            ]
-                            [ div
-                                [ Attr.class "text-base font-medium text-white"
-                                ]
-                                [ text "Tom Cook" ]
-                            , div
-                                [ Attr.class "text-sm font-medium text-gray-400"
-                                ]
-                                [ text "tom@example.com" ]
-                            ]
-                        , button
-                            [ Attr.type_ "button"
-                            , Attr.class "relative ml-auto shrink-0 rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
-                            ]
-                            [ span
-                                [ Attr.class "absolute -inset-1.5"
-                                ]
-                                []
-                            , span
-                                [ Attr.class "sr-only"
-                                ]
-                                [ text "View notifications" ]
-                            , svg
-                                [ SvgAttr.class "h-6 w-6"
-                                , SvgAttr.fill "none"
-                                , SvgAttr.viewBox "0 0 24 24"
-                                , SvgAttr.strokeWidth "1.5"
-                                , SvgAttr.stroke "currentColor"
-                                , Attr.attribute "aria-hidden" "true"
-                                , Attr.attribute "data-slot" "icon"
-                                ]
-                                [ path
-                                    [ SvgAttr.strokeLinecap "round"
-                                    , SvgAttr.strokeLinejoin "round"
-                                    , SvgAttr.d "M14.857 17.082a23.848 23.848 0 0 0 5.454-1.31A8.967 8.967 0 0 1 18 9.75V9A6 6 0 0 0 6 9v.75a8.967 8.967 0 0 1-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 0 1-5.714 0m5.714 0a3 3 0 1 1-5.714 0"
-                                    ]
-                                    []
-                                ]
-                            ]
-                        ]
-                    , div
-                        [ Attr.class "mt-3 space-y-1 px-2"
-                        ]
-                        [ a
-                            [ Attr.href "#"
-                            , Attr.class "block rounded-md px-3 py-2 text-base font-medium text-gray-400 hover:bg-gray-700 hover:text-white"
-                            ]
-                            [ text "Your Profile" ]
-                        , a
-                            [ Attr.href "#"
-                            , Attr.class "block rounded-md px-3 py-2 text-base font-medium text-gray-400 hover:bg-gray-700 hover:text-white"
-                            ]
-                            [ text "Settings" ]
-                        , a
-                            [ Attr.href "#"
-                            , Attr.class "block rounded-md px-3 py-2 text-base font-medium text-gray-400 hover:bg-gray-700 hover:text-white"
-                            ]
-                            [ text "Sign out" ]
-                        ]
-                    ]
+                , mobileNotificationsAndProfile
                 ]
             ]
         , header
@@ -405,4 +328,86 @@ link msg label =
         [ Attr.class (textColor ++ " mt-4 text-center text-sm underline hover:no-underline")
         ]
         [ renderDefaultTextLink msg label
+        ]
+
+
+mobileNotificationsAndProfile : Html msg
+mobileNotificationsAndProfile =
+    div
+        [ Attr.class "border-t border-gray-700 pb-3 pt-4"
+        ]
+        [ div
+            [ Attr.class "flex items-center px-5"
+            ]
+            [ div
+                [ Attr.class "shrink-0"
+                ]
+                [ img
+                    [ Attr.class "h-10 w-10 rounded-full"
+                    , Attr.src "https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                    , Attr.alt ""
+                    ]
+                    []
+                ]
+            , div
+                [ Attr.class "ml-3"
+                ]
+                [ div
+                    [ Attr.class "text-base font-medium text-white"
+                    ]
+                    [ text "Tom Cook" ]
+                , div
+                    [ Attr.class "text-sm font-medium text-gray-400"
+                    ]
+                    [ text "tom@example.com" ]
+                ]
+            , button
+                [ Attr.type_ "button"
+                , Attr.class "relative ml-auto shrink-0 rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
+                ]
+                [ span
+                    [ Attr.class "absolute -inset-1.5"
+                    ]
+                    []
+                , span
+                    [ Attr.class "sr-only"
+                    ]
+                    [ text "View notifications" ]
+                , svg
+                    [ SvgAttr.class "h-6 w-6"
+                    , SvgAttr.fill "none"
+                    , SvgAttr.viewBox "0 0 24 24"
+                    , SvgAttr.strokeWidth "1.5"
+                    , SvgAttr.stroke "currentColor"
+                    , Attr.attribute "aria-hidden" "true"
+                    , Attr.attribute "data-slot" "icon"
+                    ]
+                    [ path
+                        [ SvgAttr.strokeLinecap "round"
+                        , SvgAttr.strokeLinejoin "round"
+                        , SvgAttr.d "M14.857 17.082a23.848 23.848 0 0 0 5.454-1.31A8.967 8.967 0 0 1 18 9.75V9A6 6 0 0 0 6 9v.75a8.967 8.967 0 0 1-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 0 1-5.714 0m5.714 0a3 3 0 1 1-5.714 0"
+                        ]
+                        []
+                    ]
+                ]
+            ]
+        , div
+            [ Attr.class "mt-3 space-y-1 px-2"
+            ]
+            [ a
+                [ Attr.href "#"
+                , Attr.class "block rounded-md px-3 py-2 text-base font-medium text-gray-400 hover:bg-gray-700 hover:text-white"
+                ]
+                [ text "Your Profile" ]
+            , a
+                [ Attr.href "#"
+                , Attr.class "block rounded-md px-3 py-2 text-base font-medium text-gray-400 hover:bg-gray-700 hover:text-white"
+                ]
+                [ text "Settings" ]
+            , a
+                [ Attr.href "#"
+                , Attr.class "block rounded-md px-3 py-2 text-base font-medium text-gray-400 hover:bg-gray-700 hover:text-white"
+                ]
+                [ text "Sign out" ]
+            ]
         ]
