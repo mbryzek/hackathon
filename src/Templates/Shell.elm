8a7522d0d31d7a4513c91258c1bb82ef78b85f3b
Michael Bryzek <mbryzek@alum.mit.edu>
2025-02-12 10:29:49 -0500

    wip

--- src/Templates/Shell.elm
+++ src/Templates/Shell.elm
@@ -58,16 +58,6 @@ toggleMenuState state =
             Open
 
 
-enableNotifications : Bool
-enableNotifications =
-    False
-
-
-enableProfileDropdown : Bool
-enableProfileDropdown =
-    False
-
-
 type alias Section =
     { href : String, name : String }
 
@@ -128,10 +118,7 @@ navLink currentUrl section =
 logo : ViewProps msg -> Html msg
 logo props =
     div
-        [ Attr.class "shrink-0"
-        
-        ]
-        [ img
+        [ Attr.class "shrink-0" ] [ img
             [ Attr.class "h-12 w-36"
             , Attr.src logoSrc
             , Attr.alt "2025 Bergen Tech Hackathon"
@@ -140,131 +127,6 @@ logo props =
             []
         ]
     |> Html.map props.onShellMsg
-
-gated : Bool -> Html msg -> List (Html msg)
-gated enabled content =
-    if enabled then
-        [ content ]
-
-    else
-        []
-
-
-notificationsAndProfile : Html msg
-notificationsAndProfile =
-    let
-        contents : List (Html msg)
-        contents =
-            List.concat
-                [ gated enableNotifications notifications
-                , gated enableProfileDropdown profileDropdown
-                ]
-    in
-    if List.isEmpty contents then
-        text ""
-
-    else
-        div [ Attr.class "ml-4 flex items-center md:ml-6" ] contents
-
-
-notifications : Html msg
-notifications =
-    button
-        [ Attr.type_ "button"
-        , Attr.class "relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
-        ]
-        [ span
-            [ Attr.class "absolute -inset-1.5"
-            ]
-            []
-        , srOnly "View notifications"
-        , svg
-            [ SvgAttr.class "h-6 w-6"
-            , SvgAttr.fill "none"
-            , SvgAttr.viewBox "0 0 24 24"
-            , SvgAttr.strokeWidth "1.5"
-            , SvgAttr.stroke "currentColor"
-            , Attr.attribute "aria-hidden" "true"
-            , Attr.attribute "data-slot" "icon"
-            ]
-            [ path
-                [ SvgAttr.strokeLinecap "round"
-                , SvgAttr.strokeLinejoin "round"
-                , SvgAttr.d "M14.857 17.082a23.848 23.848 0 0 0 5.454-1.31A8.967 8.967 0 0 1 18 9.75V9A6 6 0 0 0 6 9v.75a8.967 8.967 0 0 1-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 0 1-5.714 0m5.714 0a3 3 0 1 1-5.714 0"
-                ]
-                []
-            ]
-        ]
-
-
-profileDropdown : Html msg
-profileDropdown =
-    div [ Attr.class "relative ml-3" ]
-        [ div []
-            [ button
-                [ Attr.type_ "button"
-                , Attr.class "relative flex max-w-xs items-center rounded-full bg-gray-800 text-sm text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
-                , Attr.id "user-menu-button"
-                , Attr.attribute "aria-expanded" "false"
-                , Attr.attribute "aria-haspopup" "true"
-                ]
-                [ span
-                    [ Attr.class "absolute -inset-1.5"
-                    ]
-                    []
-                , srOnly "Open user menu"
-                , img
-                    [ Attr.class "h-8 w-8 rounded-full"
-                    , Attr.src "https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                    , Attr.alt ""
-                    ]
-                    []
-                ]
-            ]
-        , {-
-             Dropdown menu, show/hide based on menu state.
-
-             Entering: "transition ease-out duration-100"
-             From: "transform opacity-0 scale-95"
-             To: "transform opacity-100 scale-100"
-             Leaving: "transition ease-in duration-75"
-             From: "transform opacity-100 scale-100"
-             To: "transform opacity-0 scale-95"
-          -}
-          div
-            [ Attr.class "absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
-            , Attr.attribute "role" "menu"
-            , Attr.attribute "aria-orientation" "vertical"
-            , Attr.attribute "aria-labelledby" "user-menu-button"
-            , Attr.tabindex -1
-            ]
-            [ {- Active: "bg-gray-100 outline-none", Not Active: "" -}
-              a
-                [ Attr.href "#"
-                , Attr.class "block px-4 py-2 text-sm text-gray-700"
-                , Attr.attribute "role" "menuitem"
-                , Attr.tabindex -1
-                , Attr.id "user-menu-item-0"
-                ]
-                [ text "Your Profile" ]
-            , a
-                [ Attr.href "#"
-                , Attr.class "block px-4 py-2 text-sm text-gray-700"
-                , Attr.attribute "role" "menuitem"
-                , Attr.tabindex -1
-                , Attr.id "user-menu-item-1"
-                ]
-                [ text "Settings" ]
-            , a
-                [ Attr.href "#"
-                , Attr.class "block px-4 py-2 text-sm text-gray-700"
-                , Attr.attribute "role" "menuitem"
-                , Attr.tabindex -1
-                , Attr.id "user-menu-item-2"
-                ]
-                [ text "Sign out" ]
-            ]
-        ]
 
 
 render : ViewProps msg -> String -> List (Html msg) -> Browser.Document msg
@@ -295,20 +157,9 @@ renderBody props title contents =
                         [ logo props
                         , topNavSections props.global.url
                         ]
-                    , div
-                        [ Attr.class "hidden md:block"
-                        ]
-                        [ notificationsAndProfile
-                        ]
                     , mobileMenuButton props.shellModel props
                     ]
                 ]
-            , {- Mobile menu, show/hide based on menu state. -}
-              div
-                [ Attr.class "md:hidden"
-                , Attr.id "mobile-menu"
-                ]
-                [ mobileNotificationsAndMenu props.shellModel props.global.url ]
             ]
         , header
             [ Attr.class "bg-white shadow-sm"
@@ -327,88 +178,6 @@ renderBody props title contents =
                 [ Attr.class "mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8"
                 ]
                 contents
-            ]
-        ]
-
-
-mobileNotificationsAndMenu : Model -> Url.Url -> Html msg
-mobileNotificationsAndMenu model currentUrl =
-    let
-        showMobileMenu : Bool
-        showMobileMenu =
-            case model.mobileMenuState of
-                Open ->
-                    True
-
-                Closed ->
-                    False
-
-        contents : List (Html msg)
-        contents =
-            List.concat
-                [ gated enableNotifications mobileNotifications
-                , gated showMobileMenu (mobileMenu currentUrl)
-                ]
-    in
-    if List.isEmpty contents then
-        text ""
-
-    else
-        div [ Attr.class "border-t border-gray-700 pb-3 pt-4" ] contents
-
-
-mobileNotifications : Html msg
-mobileNotifications =
-    div
-        [ Attr.class "flex items-center px-5"
-        ]
-        [ div
-            [ Attr.class "shrink-0"
-            ]
-            [ img
-                [ Attr.class "h-10 w-10 rounded-full"
-                , Attr.src "https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                , Attr.alt ""
-                ]
-                []
-            ]
-        , div
-            [ Attr.class "ml-3"
-            ]
-            [ div
-                [ Attr.class "text-base font-medium text-white"
-                ]
-                [ text "Tom Cook" ]
-            , div
-                [ Attr.class "text-sm font-medium text-gray-400"
-                ]
-                [ text "tom@example.com" ]
-            ]
-        , button
-            [ Attr.type_ "button"
-            , Attr.class "relative ml-auto shrink-0 rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
-            ]
-            [ span
-                [ Attr.class "absolute -inset-1.5"
-                ]
-                []
-            , srOnly "View notifications"
-            , svg
-                [ SvgAttr.class "h-6 w-6"
-                , SvgAttr.fill "none"
-                , SvgAttr.viewBox "0 0 24 24"
-                , SvgAttr.strokeWidth "1.5"
-                , SvgAttr.stroke "currentColor"
-                , Attr.attribute "aria-hidden" "true"
-                , Attr.attribute "data-slot" "icon"
-                ]
-                [ path
-                    [ SvgAttr.strokeLinecap "round"
-                    , SvgAttr.strokeLinejoin "round"
-                    , SvgAttr.d "M14.857 17.082a23.848 23.848 0 0 0 5.454-1.31A8.967 8.967 0 0 1 18 9.75V9A6 6 0 0 0 6 9v.75a8.967 8.967 0 0 1-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 0 1-5.714 0m5.714 0a3 3 0 1 1-5.714 0"
-                    ]
-                    []
-                ]
             ]
         ]
 
