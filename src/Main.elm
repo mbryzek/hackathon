e67200d8487fc30bffd6c6f9f19c8c90cd483474
Michael Bryzek <mbryzek@alum.mit.edu>
2025-02-12 09:52:43 -0500

    wip

--- src/Main.elm
+++ src/Main.elm
@@ -1,20 +1,16 @@
-module Main exposing (..)
+module Main exposing (Flags, Model, Msg, main)
 
-import Browser exposing (Document, UrlRequest)
+import Browser
 import Browser.Navigation as Nav
-import Global exposing (GlobalState, SessionState(..))
-import Html exposing (Html)
-import Page.Contact as PageContact
-import Page.Donate as PageDonate
-import Page.Index as PageIndex
-import Page.Sponsors as PageSponsors
-import Page.Y24.Index as PageY24Index
-import Page.Y24.Photos as PageY24Photos
-import Route
-import Templates.Shell as ShellTemplate exposing (renderShell)
-import Ui.Elements exposing (link)
+import Global exposing (GlobalState(..), GlobalStateAuthenticatedData, MainUpdatePropsWithSession, MainUpdatePropsWithLoggedIn, MainUpdatePropsWithLoggedOut, MainViewProps, Session, UpdateWithLoggedInProps, UpdateWithSessionProps)
+import Html as H
+import Util.Task as Task
+import Loading
+import NotAuthorized
+import NotFound
+import Route exposing (Route)
+import Templates.Shell as Shell
 import Url
-import Urls
 import Page.Contact as PageContact
 import Page.Donate as PageDonate
 import Page.Index as PageIndex
@@ -24,207 +20,288 @@ import Page.Y24.Photos as PageY24Photos
 
 
 type alias Flags =
-    {}
+    { sessionId : Maybe String }
 
 
-main : Program Flags Model MainMsg
+main : Program Flags Model Msg
 main =
     Browser.application
         { init = init
-        , update = update
         , view = view
+        , update = update
         , subscriptions = subscriptions
-        , onUrlRequest = \v -> InternalMsg (LinkClicked v)
-        , onUrlChange = \v -> InternalMsg (UrlChanged v)
+        , onUrlChange = UrlChanged
+        , onUrlRequest = LinkClicked
         }
 
 
-type alias Model =
-    { session : GlobalState
-    , state : IntermediateState
-    , shell : ShellTemplate.Model
+type Model
+    = Init InitModel
+    | Ready ReadyModel
+
+
+type alias InitModel =
+    { sessionId : String
     , url : Url.Url
-    , page : Maybe Page
+    , key : Nav.Key
     }
 
 
-type alias IntermediateState =
-    { navKey : Nav.Key }
+type alias ReadyModel =
+    { global : GlobalState
+    , shellModel : Shell.Model
+    , page : Page
+    }
 
 
-init : Flags -> Url.Url -> Nav.Key -> ( Model, Cmd MainMsg )
-init _ url navKey =
-    let
-        state : IntermediateState
-        state =
-            { navKey = navKey }
-
-        ( shell, shellCmd ) =
-            ShellTemplate.init navKey
-
-        initModel : Model
-        initModel =
-            { session = { session = SessionLoggedOut, navKey = navKey }
-            , state = state
-            , shell = shell
-            , page = Nothing
-            , url = url
-            }
-
-        ( pageModel, pageCmd ) =
-            renderPage initModel
-    in
-    ( pageModel, Cmd.batch [ Cmd.map (\m -> InternalMsg (ShellTemplateMsg m)) shellCmd, pageCmd ] )
-
-
-renderPage : Model -> ( Model, Cmd MainMsg )
-renderPage model =
-    case parseUrl model.state model.url of
-        Just ( p, c ) ->
-            ( { model | page = Just p }, c )
-
-        Nothing ->
-            ( { model | page = Nothing }, Cmd.none )
-
-
-type MainInternalMsg
-    = LinkClicked UrlRequest
-    | UrlChanged Url.Url
-    | RedirectTo String
-    | ShellTemplateMsg ShellTemplate.ShellMsg
-
-
-type MainMsg
-    = InternalMsg MainInternalMsg
-    | PageMsg MainPageMsg
-
-type MainPageMsg = 
-    Noop
-
-update : MainMsg -> Model -> ( Model, Cmd MainMsg )
-update msg model =
-    case msg of
-        InternalMsg m ->
-            handleInternalMsg m model
-
-        PageMsg m ->
-            handlePageMsg m model
-
-handlePageMsg : MainInternalMsg -> Model -> ( Model, Cmd MainMsg )
-handlePageMsg msg model =
-    case msg of 
-        Noop ->
-            ( model, Cmd.none )
-
-handleInternalMsg : MainInternalMsg -> Model -> ( Model, Cmd MainMsg )
-handleInternalMsg msg model =
-    case msg of
-        UrlChanged url ->
-            case parseUrl model.state url of
-                Nothing ->
-                    ( model, Cmd.none )
-
-                Just ( p, cmd ) ->
-                    ( { model | page = Just p }, cmd )
-
-        RedirectTo u ->
-            ( model, Nav.pushUrl model.state.navKey u )
-
-        ShellTemplateMsg subMsg ->
-            let
-                ( updatedShell, shellCmd ) =
-                    ShellTemplate.update subMsg model.shell
-            in
-            ( { model | shell = updatedShell }
-            , Cmd.map (\m -> InternalMsg (ShellTemplateMsg m)) shellCmd
+init : Flags -> Url.Url -> Nav.Key -> ( Model, Cmd Msg )
+init flags url key =
+    case flags.sessionId of
+        Just sessionId ->
+            ( Init { sessionId = sessionId, url = url, key = key }
+            , Task.dispatch (InitMsg (SimluateCallToLogin sessionId))
             )
 
-        LinkClicked urlRequest ->
+        Nothing ->
+            initWithGlobal (toGlobalState key Nothing) url
+
+
+initWithGlobal : GlobalState -> Url.Url -> ( Model, Cmd Msg )
+initWithGlobal global url =
+    let
+        ( shellModel, shellCmd ) =
+            Shell.init global
+
+        ( page, cmd ) =
+            Route.fromUrl url
+                |> getPageFromRoute global
+    in
+    ( Ready
+        { global = global
+        , shellModel = shellModel
+        , page = page
+        }
+    , Cmd.batch
+        [ Cmd.map (ReadyMsg << ChangedPage) cmd
+        , Cmd.map (ReadyMsg << ChangedInternal << ShellMsg) shellCmd
+        ]
+    )
+
+
+type Msg
+    = InitMsg InitMsg
+    | ReadyMsg ReadyMsg
+    | LinkClicked Browser.UrlRequest
+    | UrlChanged Url.Url
+
+
+type ReadyMsg
+    = ChangedInternal InternalMsg
+    | ChangedPage PageMsg
+
+
+type InitMsg
+    = SimluateCallToLogin String
+
+
+type InternalMsg
+    = ShellMsg Shell.Msg
+    | LoggedInMsg UpdateWithLoggedInProps
+    | LoggedOutMsg
+    | SessionUpdatedMsg UpdateWithSessionProps
+
+
+redirectTo : Model -> String -> Cmd Msg
+redirectTo model url =
+    let
+        key : Nav.Key
+        key =
+            case model of
+                Init data ->
+                    data.key
+
+                Ready rm ->
+                    Global.getNavKey rm.global
+    in
+    Nav.pushUrl key url
+
+
+updateSession : ReadyModel -> Maybe Session -> ReadyModel
+updateSession model session =
+    updateShell { model | global = toGlobalState (Global.getNavKey model.global) session }
+
+
+toGlobalState : Nav.Key -> Maybe Session -> GlobalState
+toGlobalState key maybeSession =
+    case maybeSession of
+        Just u ->
+            GlobalStateAuthenticated { navKey = key, session = u }
+
+        Nothing ->
+            GlobalStateAnonymous { navKey = key }
+
+
+updateShell : ReadyModel -> ReadyModel
+updateShell model =
+    let
+        shellModel : Shell.Model
+        shellModel =
+            model.shellModel
+    in
+    { model | shellModel = { shellModel | global = model.global } }
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case ( msg, model ) of
+        ( LinkClicked urlRequest, _ ) ->
             case urlRequest of
                 Browser.Internal url ->
-                    ( model
-                    , Nav.pushUrl model.state.navKey (Url.toString url)
-                    )
+                    ( model, redirectTo model (Url.toString url) )
 
-                Browser.External url ->
-                    ( model
-                    , Nav.load url
-                    )
+                Browser.External href ->
+                    ( model, Nav.load href )
+
+        ( UrlChanged url, Ready readyModel ) ->
+            let
+                ( page, cmd ) =
+                    Route.fromUrl url
+                        |> getPageFromRoute readyModel.global
+            in
+            ( Ready { readyModel | page = page }
+            , Cmd.map (ReadyMsg << ChangedPage) cmd
+            )
+
+        ( UrlChanged url, Init data ) ->
+            ( Init { data | url = url }, Cmd.none )
+
+        ( InitMsg initMsg, Init initModel ) ->
+            updateInit initMsg initModel
+
+        ( InitMsg _, Ready _ ) ->
+            ( model, Cmd.none )
+
+        ( ReadyMsg readyMsg, Ready readyModel ) ->
+            updateReady readyMsg readyModel |> Tuple.mapFirst (\m -> Ready m)
+
+        ( ReadyMsg _, Init _ ) ->
+            ( model, Cmd.none )
 
 
-view : Model -> Document MainMsg
+updateInit : InitMsg -> InitModel -> ( Model, Cmd Msg )
+updateInit msg model =
+    case msg of
+        SimluateCallToLogin sessionId ->
+            let
+                newSession : Session
+                newSession =
+                    { id = sessionId, user = { name = "John Doe" } }
+            in
+            initWithGlobal (toGlobalState model.key (Just newSession)) model.url
+
+
+updateReady : ReadyMsg -> ReadyModel -> ( ReadyModel, Cmd Msg )
+updateReady msg model =
+    case msg of
+        ChangedInternal internalMsg ->
+            updateInternal internalMsg model
+
+        ChangedPage pageMsg ->
+            updatePage model pageMsg
+                |> Tuple.mapFirst (\page -> { model | page = page })
+
+
+updateInternal : InternalMsg -> ReadyModel -> ( ReadyModel, Cmd Msg )
+updateInternal msg model =
+    case msg of
+        ShellMsg shellMsg ->
+            Shell.update shellMsg model.shellModel
+                |> Tuple.mapFirst (\m -> { model | shellModel = m })
+                |> Tuple.mapSecond (Cmd.map (ReadyMsg << ChangedInternal << ShellMsg))
+
+        LoggedInMsg { redirectUrl, session } ->
+            ( updateSession model (Just session), redirectTo (Ready model) redirectUrl )
+
+        SessionUpdatedMsg { redirectUrl, session } ->
+            let
+                redirectCmd : Cmd Msg
+                redirectCmd =
+                    case redirectUrl of
+                        Just url ->
+                            redirectTo (Ready model) url
+
+                        Nothing ->
+                            Cmd.none
+            in
+            Debug.log ("Updating session")
+            ( updateSession model (Just session), redirectCmd )
+
+        LoggedOutMsg ->
+            ( updateSession model Nothing, Cmd.none )
+
+
+view : Model -> Browser.Document Msg
 view model =
-    { title = "BT Hackathon"
-    , body =
-        [ case model.page of
-            Nothing ->
-                viewNotFound model
+    case model of
+        Init _ ->
+            Debug.log ("Main.view iwth init model")
+            Loading.view
 
-            Just p ->
-                pageView p
-        ]
+        Ready readyModel ->
+            Debug.log ("Main.view iwth ready model")
+            viewReady readyModel
+
+
+updatePropsWithLoggedIn : (a -> PageMsg) -> MainUpdatePropsWithLoggedIn a Msg
+updatePropsWithLoggedIn pageMsg =
+    { onLoggedIn = ReadyMsg << ChangedInternal << LoggedInMsg
+    , msgMap = ReadyMsg << ChangedPage << pageMsg 
+    }
+
+updatePropsWithLoggedOut : (a -> PageMsg) -> MainUpdatePropsWithLoggedOut a Msg
+updatePropsWithLoggedOut pageMsg =
+    { onLoggedOut = ReadyMsg (ChangedInternal LoggedOutMsg), msgMap = ReadyMsg << ChangedPage << pageMsg }
+
+updatePropsWithSessionUpdate : (a -> PageMsg) -> MainUpdatePropsWithSession a Msg
+updatePropsWithSessionUpdate pageMsg =
+    { onSessionUpdate = ReadyMsg << ChangedInternal << SessionUpdatedMsg
+    , msgMap = ReadyMsg << ChangedPage << pageMsg 
+    }
+
+shellViewProps : ReadyModel -> Shell.ViewProps Msg
+shellViewProps model =
+    { shellModel = model.shellModel
+    , onShellMsg = ReadyMsg << ChangedInternal << ShellMsg
     }
 
 
-viewNotFound : Model -> Html MainMsg
-viewNotFound model =
-    renderErrorPage model
-        { title = "Page not found"
-        , body = "The page you are looking for does not exist."
-        , link = Just (link (RedirectTo Urls.index) "Go to the home page")
-        }
+mainViewProps : GlobalState -> (a -> PageMsg) -> MainViewProps a Msg
+mainViewProps global pageMsg =
+    { global = global, msgMap = ReadyMsg << ChangedPage << pageMsg }
 
 
-type alias ErrorPageParams =
-    { title : String
-    , body : String
-    , link : Maybe (Html MainInternalMsg)
+pageAuthenticatedData : GlobalState -> (GlobalStateAuthenticatedData -> ( Page, Cmd PageMsg )) -> ( Page, Cmd PageMsg )
+pageAuthenticatedData global f =
+    case global of
+        GlobalStateAuthenticated data ->
+            f data
+
+        _ ->
+            ( PageNotAuthorized, Nav.pushUrl (Global.getNavKey global) "/login" )
+
+
+mapDoc : (a -> PageMsg) -> Browser.Document a -> Browser.Document Msg
+mapDoc pageMsg doc =
+    { title = doc.title
+    , body = List.map (H.map (ReadyMsg << ChangedPage << pageMsg)) doc.body
     }
 
-
-renderErrorPage : Model -> ErrorPageParams -> Html MainMsg
-renderErrorPage model params =
-    let
-        htmlLink : List (Html MainMsg)
-        htmlLink =
-            case params.link of
-                Nothing ->
-                    []
-
-                Just l ->
-                    [ Html.map InternalMsg l ]
-    in
-    renderShell model.shell
-        (\m -> InternalMsg (ShellTemplateMsg m))
-        { title = params.title
-        , url = Nothing
-        }
-        (List.append
-            [ Html.p [] [ Html.text params.body ] ]
-            htmlLink
-        )
+subscriptions : Model -> Sub Msg
+subscriptions _ =
+    pageSubscriptions |> Sub.map (ReadyMsg << ChangedPage)
 
 
-type alias Session =
-    { id : String }
-
-
-parseUrl : IntermediateState -> Url.Url -> Maybe ( Page, Cmd MainMsg )
-parseUrl state url =
-    Maybe.map (toPage state Nothing) (Route.parseUrl url)
-
-
-toGlobalState : IntermediateState -> Maybe Session -> GlobalState
-toGlobalState state _ =
-    { session = SessionLoggedOut
-    , navKey = state.navKey
-    }
-
-subscriptions : Sub PageMsg
-subscriptions =
-    pageSubscriptions
-
-
+-- Organized so the below can be code generated
 -- CODEGEN START
 pageSubscriptions : Sub PageMsg
 pageSubscriptions =
