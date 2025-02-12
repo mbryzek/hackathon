d67f679c9d1a09dc28d6c55f7418b7498dc0a42d
Michael Bryzek <mbryzek@alum.mit.edu>
2025-02-12 09:54:15 -0500

    wip

--- src/Templates/SimplePage.elm
+++ src/Templates/SimplePage.elm
@@ -11,6 +11,7 @@ render title contents =
     , body = [
         div [ class "flex flex-col items-center justify-center h-screen" ]
             [ div [ class "text-2xl font-bold" ] [ text title ]
-            , div [ class "mt-4" ] contents
+            , div [ class "mt-4" ] [ contents ]
         ]
+    ]
     }
