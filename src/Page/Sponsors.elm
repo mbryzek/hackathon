e6e9ca8a342e80974828bc1c30ef1524c846485d
Michael Bryzek <mbryzek@alum.mit.edu>
2025-02-12 10:19:20 -0500

    wip

--- src/Page/Sponsors.elm
+++ src/Page/Sponsors.elm
@@ -1,44 +1,17 @@
 module Page.Sponsors exposing (view)
 
-import Global exposing (GlobalState)
-import Html exposing (Html)
+import Browser
 import Templates.PhotoGallery exposing (renderPhotoGallery)
-import Templates.Shell as ShellTemplate exposing (renderShell)
+import Templates.Shell as Shell
 import Ui.Elements exposing (p)
-import Urls
 
 
-type alias Model =
-    { global : GlobalState
-    , shell : ShellTemplate.Model }
-
-
-type msg =
-    ShellTemplatemsg ShellTemplate.Shellmsg
-
-
-init : ( Model, Cmd msg )
-init global =
-    let
-        ( shell, shellCmd ) =
-            
-    in
-    ( { global = global
-      , shell = shell
-      }
-    , Cmd.map ShellTemplatemsg shellCmd
-    )
-
-
-update : msg -> Model -> ( Model, Cmd msg )
-update msg model =
-    case msg of
-        ShellTemplatemsg submsg ->
-            let
-                ( updatedShell, shellCmd ) =
-                    ShellTemplate.update submsg model.shell
-            in
-            ( { model | shell = updatedShell }, Cmd.map ShellTemplatemsg shellCmd )
+view : Shell.ViewProps msg -> Browser.Document msg
+view props =
+    Shell.render props "2024 Sponsors" [
+        p "A huge thank you to our sponsors who made our 2024 Hackathon possible!"
+        , renderPhotoGallery logos
+    ]
 
 
 toUrl : String -> String
@@ -58,14 +31,4 @@ logos =
         , "WCCG_Logo_final.jpg"
         , "pan.jpg"
         , "Bryzek Logo V2.1.jpg"
-        ]
-
-
-view : GlobalState -> Html msg
-view global =
-    renderShell model.shell ShellTemplatemsg {
-        title = "2024 Sponsors", url = Just Urls.sponsors
-    }
-        [ p "A huge thank you to our sponsors who made our 2024 Hackathon possible!"
-        , renderPhotoGallery logos
         ]
