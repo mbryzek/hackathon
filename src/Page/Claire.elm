ba17044409a3937af1bc96bd98b87b28b5c0ad45
Michael Bryzek <mbryzek@alum.mit.edu>
2025-02-14 20:44:35 -0500

    wip

--- src/Page/Claire.elm
+++ src/Page/Claire.elm
@@ -13,12 +13,20 @@ view props =
         [ div [ class "max-w-3xl mx-auto py-8" ]
             [ h2 [ class "text-2xl font-bold text-amber-800 mb-4" ]
                 [ Html.text "Welcome to Claire's Page" ]
-            , p [ class "mb-4" ]
-                [ Html.text "This is Claire's new page. Content coming soon!" ]
             , img [ src "https://github.com/mbryzek/hackathon-photos/blob/main/claire/head.jpg?raw=true"
                   , alt "Claire's photo"
                   , style "width" "200px"
                   , style "border-radius" "50%"
+                  ] []
+            , p [ class "mt-4 mb-4 text-lg" ]
+                [ Html.text "Exciting news! I'm heading to Cancun tomorrow for some sun and relaxation! I'll be staying at the GR Solaris Cancun resort. 🌴☀️" ]
+            , img [ src "https://images.unsplash.com/photo-1552074284-5e88ef1aef18"
+                  , alt "Beautiful Cancun beach with turquoise water and white sand"
+                  , class "w-full max-w-lg rounded-lg shadow-lg mt-2 mx-auto block"
+                  ] []
+            , img [ src "https://images.trvl-media.com/lodging/2000000/1120000/1110100/1110099/1d5a8cd9.jpg?impolicy=resizecrop&rw=1200&ra=fit"
+                  , alt "GR Solaris Cancun resort"
+                  , class "w-full max-w-lg rounded-lg shadow-lg mt-4 mx-auto block"
                   ] []
             ]
         ] 
