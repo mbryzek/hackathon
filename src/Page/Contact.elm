4dd40d0d5a66d0121108362c085a760d75843017
Michael Bryzek <mbryzek@alum.mit.edu>
2025-12-13 11:36:50 -0500

    Update contact info

--- src/Page/Contact.elm
+++ src/Page/Contact.elm
@@ -1,4 +1,6 @@
 -- codegen.global.state: GlobalStateAnonymousData
+
+
 module Page.Contact exposing (view)
 
 import Browser
@@ -7,14 +9,16 @@ import Html.Attributes exposing (class)
 import Templates.Shell as Shell
 import Ui.Elements exposing (p, textDiv)
 
+
 view : Shell.ViewProps msg -> Browser.Document msg
 view props =
-    Shell.render props "Contact The Hackathon Organizers" [
-        textDiv
-            [ p "The Bergen Tech Hackathon is run by a group of volunteers. If you have any questions, please contact:"
+    Shell.render props
+        "Contact The Hackathon Organizers"
+        [ textDiv
+            [ p "The Bergen Tech Hackathon is run by Bergen Youth Enrichment, a 501(c) 3 non-profit organization which is 100% volunteer-run. If you have any questions, please contact:"
             , div [ class "pl-4" ]
                 [ p "Michael Bryzek"
-                , p "mbryzek@gmail.com"
+                , p "bergenyouthenrichment@gmail.com"
                 ]
             ]
-    ]
+        ]
