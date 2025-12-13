1e080f7408c7735a7b110892a18736e888ac935f
Michael Bryzek <mbryzek@alum.mit.edu>
2025-12-13 12:50:03 -0500

    wip

--- src/routes/Y24/sponsors/+page.svelte
+++ src/routes/Y24/sponsors/+page.svelte
@@ -1,7 +1,14 @@
 <script lang="ts">
 	import Shell from '$lib/components/Shell.svelte';
+	import PhotoGallery from '$lib/components/PhotoGallery.svelte';
+	import { sponsorsY24 } from '$lib/data/sponsors-y24';
 </script>
 
 <Shell title="2024 Sponsors">
-	<p class="text-gray-800 font-light">Coming soon!</p>
+	<div class="max-w-4xl mx-auto">
+		<p class="text-gray-800 font-light text-lg mb-6">
+			A huge thank you to our sponsors who made our 2024 Hackathon possible!
+		</p>
+		<PhotoGallery photos={sponsorsY24} />
+	</div>
 </Shell>
