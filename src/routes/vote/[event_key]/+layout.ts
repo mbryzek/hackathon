e63ab577c919ac05290acc4c399179f7fa2e88a5
Michael Bryzek <mbryzek@alum.mit.edu>
2026-01-07 17:37:02 -0500

    Disable prerender

--- src/routes/vote/[event_key]/+layout.ts
+++ src/routes/vote/[event_key]/+layout.ts
@@ -1,6 +1,9 @@
 import type { LayoutLoad } from './$types';
 import { voteApi } from '$lib/api/client';
 
+// Dynamic routes cannot be prerendered
+export const prerender = false;
+
 export const load: LayoutLoad = async ({ params }) => {
 	const response = await voteApi.getOpenEvents();
 	const event = response.data?.find(e => e.key === params.event_key) ?? null;
